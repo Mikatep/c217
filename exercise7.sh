d8740e3194545ffc52d9db619cae197fab1efbd7
Mikatep <miriamcordobacapote@gmail.com>
2022-04-21 11:29:59 +0000

    some more exercises

--- exercise7.sh
+++ exercise7.sh
@@ -1,1 +1,40 @@
 #!/bin/sh
+
+echo "Welcome to the True or False test about UK and life in the UK"
+echo "Please enter true or false in every single one of the questions"
+
+read -p "London is the capital of United Kingdom:" a 
+if $a == true ;then echo "You got it right!";else echo "You made a mistake";fi
+read -p "London can be found in the North east of United Kingdom:" b
+if $b == false ;then echo "You got it right!";else echo "You made a mistake";fi
+read -p "The current queen of England is Elizabeth the II:" c
+if $c == true ;then echo "You got it right!";else echo "You made a mistake";fi
+read -p "Uk is well Known for it's tropical climate:" d
+if $d == false ;then echo "You got it right!";else echo "You made a mistake";fi
+read -p "London has a population of almost 2 million people:" e
+if $e == false ;then echo "You got it right!";else echo "You made a mistake";fi
+read -p "London is one of the most affordable capitals in Europe:" f
+if $f == false ;then echo "You got it right!";else echo "You made a mistake";fi
+read -p "United kingdom is located in the North hemisfere:" g
+if $g == true ;then echo "You got it right!";else echo "You made a mistake";fi
+read -p "The chesire palace is the main residency of the british monarchs:" h
+if $h == false ;then echo "You got it right!";else echo "You made a mistake";fi
+read -p "There're 3 airports in London:" i
+if $i == false ;then echo "You got it right!";else echo "You made a mistake";fi
+read -p "The official language in England is french:" j
+if $j == false ;then echo "You got it right!";else echo "You made a mistake";fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
